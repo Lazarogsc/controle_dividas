42726d690995e2c97249ed534bf3e93fe17603ef
jhonatanmsc <jhonmscosta@gmail.com>
2024-12-17 18:36:35 -0300

    adicionado arquivo de constantes

---
+++ constants.py
@@ -0,0 +1,15 @@
+DATABASE_PATH = 'dividas.db'
+MONTHS = [
+    "Janeiro", 
+    "Fevereiro", 
+    "Março", 
+    "Abril", 
+    "Maio", 
+    "Junho", 
+    "Julho", 
+    "Agosto", 
+    "Setembro", 
+    "Outubro", 
+    "Novembro", 
+    "Dezembro"
+]
